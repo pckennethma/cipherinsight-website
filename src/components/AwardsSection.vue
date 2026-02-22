0518829b608df5b7619bd259d976c96cfd3cb02e
Pingchuan Ma <44944354+pckennethma@users.noreply.github.com>
2026-02-22 11:47:38 +0800

    Update IEEE S&P award description

--- src/components/AwardsSection.vue
+++ src/components/AwardsSection.vue
@@ -52,7 +52,7 @@ const awards = [
   },
   {
     title: 'IEEE S&P Distinguished Paper Award',
-    detail: 'Premier computer security conference',
+    detail: 'One of the most prestigious awards in computer security',
     iconBg: 'bg-red-500/20',
     iconColor: 'text-red-400',
     detailColor: 'text-red-400',
